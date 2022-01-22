bd5ad97927ef0ce1c6938800a66bb0c3bf9f9b23
GunGameOG <58014273+GunGameOG@users.noreply.github.com>
2022-01-22 03:24:34 -0500

    discordalerts.sh
    | Experimental Update | 
    
    - Changed some stuff.
    - Added Packets Per Second.
    - Fixed PCAP's : Create dumps folder.

--- discordalerts.sh
+++ discordalerts.sh
@@ -1,31 +1,45 @@
-echo -e "\033[32;5mStarting Attack Logs \033[0m"
+echo -e "Discord-VPN-DDoS-Attack-Alerts Software coded by GunGameOG"
+echo
+echo "If you need any assistence configuring or adding the notify message GunGameOG#9082 on discord for help."
+echo
+echo -e "033[97mPackets/s \033[36m{}\n\033[97mBytes/s \033[36m{}\n\033[97mKbp/s \033[36m{}\n\033[97mGbp/s \033[36m{}\n\033[97mMbp/s \033[36m{}"
 interface=eth0
-dumpdir=/root/TCPDumps
-capturefile=/root/TCPDumps
- url='https://discord.com/api/webhooks/847722211170189313/ENqLj9Hn9K22vFsI9T97_TJdfRPpAC7xk5ZLRb322CS9dhCETjYWRN12dsFbi7Ob0aQ4' ## Change this to your Webhook URL
+dumpdir=/root/dumps
+url='WEBHOOK HERE' ## Change this to your Webhook URL
 while /bin/true; do
-  pkt_old=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+  old_b=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $1 }'`
+  
+  old_ps=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
   sleep 1
-  pkt_new=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
- 
-  pkt=$(( $pkt_new - $pkt_old ))
-  echo -ne "\r$pkt packets/s\033[0K"
-  ## tcpdump -n -s0 -c 1000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap
-  ## echo "Detecting Attack Packets"
-  ## sleep 1
-  if [ $pkt -gt 30000 ]; then ## Attack alert will display after incoming traffic reach 30000 PPS
-    echo "Attack Detected"
+  new_b=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $1 }'`
+
+  new_ps=`grep $interface: /proc/net/dev | cut -d :  -f2 | awk '{ print $2 }'`
+  ##Defining Packets/s
+  pps=$(( $new_ps - $old_ps ))
+  ##Defining Bytes/s
+  byte=$(( $new_b - $old_b ))
+
+  gigs=$(( $byte/1024 ** 3 ))
+  mbps=$(( $byte/1024 ** 2 ))
+  kbps=$(( $byte/1024 ** 1 ))
+
+  echo -ne "\r$pps packets/s\033[0K"
+  tcpdump -n -s0 -c 1500 -w $dumpdir/capture.`date +"%Y%m%d-%H%M%S"`.pcap
+  echo "`date` Detecting Attack Packets."
+  sleep 1
+  if [ $pps -gt 10000 ]; then ## Attack alert will display after incoming traffic reach 30000 PPS
+    echo " Attack Detected Monitoring Incoming Traffic"
     curl -H "Content-Type: application/json" -X POST -d '{
       "embeds": [{
       	"inline": false,
-        "title": "Attack Detected",
-        "username": "Galaxy Gate Attack Alerts",
+        "title": "Attack Detected On",
+        "username": "Attack Alerts",
         "color": 15158332,
          "thumbnail": {
-          "url": "https://imgur.com/a/cZAa3Pu.gif"
+          "url": "https://imgur.com/a/cZAa3Pu"
         },
          "footer": {
-            "text": "New York, US",
+            "text": "Our system is attempting to mitigate the attack and automatic packet dumping has been activated.",
             "icon_url": "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-800.png"
           },
     
@@ -33,36 +47,37 @@ while /bin/true; do
          "fields": [
       {
         "name": "**Server Provider**",
-        "value": "Galaxy Gate Hosting",
+        "value": "OVH LLC",
         "inline": false
       },
       {
         "name": "**IP Address**",
-        "value": "144.172.67.x",
+        "value": "x.x.x.x",
         "inline": false
       },
       {
-        "name": "**Location**",
-        "value": "Staten Island, New York",
+        "name": "**Incoming Packets**",
+        "value": " '$pps' Pps ",
         "inline": false
       }
     ]
       }]
     }' $url
     echo "Paused for."
-    sleep 220  ## The "Attack no longer detected" alert will display in 220 seconds
+    sleep 120  && pkill -HUP -f /usr/sbin/tcpdump  ## The "Attack no longer detected" alert will display in 220 seconds
     ## echo "Traffic Attack Packets Scrubbed"
+    echo -ne "\r$mbps megabytes/s\033[97"
     curl -H "Content-Type: application/json" -X POST -d '{
       "embeds": [{
       	"inline": false,
-        "title": "Attack Filtered",
-        "username": "Galaxy Gate Attack Alerts",
+        "title": "Attack Stopped",
+        "username": "  Attack Alerts",
         "color": 3066993,
          "thumbnail": {
           "url": "https://imgur.com/a/1YNwLCo.gif"
         },
          "footer": {
-            "text": "New York, US",
+            "text": "Reston, VA",
             "icon_url": "https://cdn.countryflags.com/thumbs/united-states-of-america/flag-800.png"
           },    
           
@@ -70,17 +85,17 @@ while /bin/true; do
          "fields": [
       {
         "name": "**Server Provider**",
-        "value": "Galaxy Gate Hosting",
+        "value": "OVH LLC",
         "inline": false
       },
       {
         "name": "**IP Address**",
-        "value": "144.172.67.x",
+        "value": "x.x.x.x",
         "inline": false
       },
       {
-        "name": "**Location**",
-        "value": "Staten Island, New York",
+        "name": "**Packets**",
+        "value": "'$mbps' Mbps ",
         "inline": false
       }
     ]
